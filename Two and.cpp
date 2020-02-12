13ae18a7c0a847cfa5b823907f0381cbe2328db0
RITESG <60151464+RITESG@users.noreply.github.com>
2020-02-13 00:46:09 +0530

    Add files via upload

---
+++ Two and.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+     
+     {
+	 
+      int a,b,c;
+      printf("Enter Two Numbers");
+      c=a+b;
+      scanf("%d%d",&a&b);
+      printf("The Sum Is "%d);
+      getch();
+      
+  } 
