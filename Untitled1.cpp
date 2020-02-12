94d3d84242c8fc29f090c660d1430e20452e4608
RITESG <60151464+RITESG@users.noreply.github.com>
2020-02-13 01:00:56 +0530

    Add files via upload

---
+++ Untitled1.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+    {
+    	int a,b,c;
+    	printf("enter two numbers")
+    	scanf("%d%d",&a&b);
+    	c=a;
+    	b=a;
+    	
+    	
+    	
+    	getch();
+	}
